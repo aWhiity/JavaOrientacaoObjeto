c732dcf602fd71ba7e2645ba86e13ec0c7713ef1
Milena Alves <87149523+aWhiity@users.noreply.github.com>
2023-09-10 17:22:21 -0300

    ex de fixacao

---
+++ banco/src/Encapsulado/Exercicios/Conta.java
@@ -0,0 +1,63 @@
+package Encapsulado.Exercicios;
+/*Em um banco, para se cadastrar uma conta bancária, é necessário informar o número da conta, o nome do
+titular da conta, e o valor de depósito inicial que o titular depositou ao abrir a conta. Este valor de depósito
+inicial, entretanto, é opcional, ou seja: se o titular não tiver dinheiro a depositar no momento de abrir sua
+conta, o depósito inicial não será feito e o saldo inicial da conta será, naturalmente, zero.
+L
+Importante: uma vez que uma conta bancária, foi aberta, o número da conta nunca poderá ser alterado. Já
+o nome do titular pode ser alterado (pois uma pessoa pode mudar de nome por ocasião de casamento, por
+exemplo).
+Por fim, o saldo da conta não pode ser alterado livremente. É preciso haver um mecanismo para proteger
+isso. O saldo só aumenta por meio de depósitos, e só diminui por meio de saques. Para cada saque
+realizado, o banco cobra uma taxa de $ 5.00. Nota: a conta pode ficar com saldo negativo se o saldo não for
+suficiente para realizar o saque e/ou pagar a taxa.
+Você deve fazer um programa que realize o cadastro de uma conta, dando opção para que seja ou não
+informado o valor de depósito inicial. Em seguida, realizar um depósito e depois um saque, sempre
+mostrando os dados da conta após cada operação. */
+public class Conta {
+    private int numero;
+    private String titular;
+    private float saldo;
+
+    public Conta(int numero) {
+        this.numero = numero;
+    }
+
+    public void depositar(float valor) {
+        if(valor > 0) {
+            this.saldo+=valor;
+            return;
+        } else {
+            System.out.println("Valor invalido.");
+        }
+    }
+
+    public void sacar(float valor) {
+        this.saldo-=(valor+5);
+        System.out.println(this.saldo);
+    
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta [numero=" + numero + ", titular=" + titular + ", saldo=" + saldo + "]";
+    }
+
+    
+}
---
+++ banco/src/Encapsulado/Exercicios/InfoBanco.java
@@ -0,0 +1,28 @@
+package Encapsulado.Exercicios;
+
+import java.util.Scanner;
+
+public class InfoBanco {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o numero da conta.");
+        Conta conta = new Conta(input.nextInt());
+        input.nextLine();
+        System.out.println("Digite o nome da conta.");
+        conta.setTitular(input.nextLine());
+        System.out.println("Digite 1 se você deseja depositar um valor inicial.\n\tCaso não queira, digite qualquer outro número.");
+        int escolha = input.nextInt();
+        input.nextLine();
+        if(escolha==1) {
+            System.out.println("Qual o valor a ser depositado?(formato: 00,00)");
+            conta.depositar(input.nextFloat());
+            System.out.println("Dados atuais:");
+            System.out.println("Nome: " + conta.getTitular());
+            System.out.println("Número: " + conta.getNumero());
+            System.out.println("Saldo: " + conta.getSaldo());
+        }
+        System.out.println("Digite o valor do saque: ");
+        conta.sacar(input.nextFloat());
+        System.out.println(conta);
+    }
+}
