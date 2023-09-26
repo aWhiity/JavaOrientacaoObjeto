53273b007e4913cc9d4f3e5031825baf60689002
Milena Alves <87149523+aWhiity@users.noreply.github.com>
2023-09-26 11:37:42 -0300

    New File

---
+++ banco/src/encapsulamento/exercicios/departamentoInformatica/Disciplina.java
@@ -0,0 +1,20 @@
+package encapsulamento.exercicios.departamentoInformatica;
+
+/*O Departamento Acadêmico de Informática deseja automatizar o processo de criação de
+disciplinas para os seus cursos. Desenvolva um sistema que seja capaz de criar novas
+disciplinas. Em uma conversa com os coordenadores de cursos foi possível identificar que
+uma disciplina é constituída de seu código, nome, modalidade (Presencial ou A Distância),
+carga horária (em horas) e conteúdo programático. Cada disciplina pode exibir um relatório
+(uma espécie de resumo), ilustrando ao usuário do sistema os valores de seus dados. Para
+efetuar a instanciação de cada disciplina, atribuição de valores e invocação dos métodos,
+utilize uma classe de teste. */
+
+public class Disciplina {
+    int codigo;
+    String nome;
+    String modalidade;
+    int cargaHoraria;
+    String conteudo;
+}
+
+
