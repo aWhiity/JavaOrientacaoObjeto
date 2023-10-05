0d4f4711858a6bee51f0bf6e33e46711fdde64d6
Milena Alves <87149523+aWhiity@users.noreply.github.com>
2023-10-05 16:23:00 -0300

    fixing ex array

---
+++ udemy/src/arraylistas/Altura.java
@@ -0,0 +1,24 @@
+package arraylistas;
+
+import java.util.Scanner;
+
+public class Altura {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite a quantidade de posições: ");
+        int n = input.nextInt();
+        double[] vet = new double[n];
+        double media=0;
+        for(int i=0;i<n;i++) {
+            System.out.println("Digite o " + (i+1) + " valor");
+            vet[i] = input.nextDouble();
+            media += vet[i];
+        }
+        for(int i=0;i<n;i++) {
+            System.out.println("Valor " + (i+1) + ": " + vet[i]);
+        }
+        media = media / n;
+        System.out.println("A media das alturas é de: " + media);
+        input.close();
+    }
+}
